0d9f66ede5105c5109907de190a1aedccca800eb
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2025-01-14 16:01:56 +0200

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Dashboard from "./components/dashboard/Dashboard";
+
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
+      <Dashboard />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import './Navbar.css'
 
 const Navbar = () => {
   return (
